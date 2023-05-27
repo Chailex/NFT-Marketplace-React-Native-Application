9b4b2b5902da6f83106b67f9fdbb35fc5ebca127
chailex <chailexsarma@gmail.com>
2023-05-27 12:34:59 +0530

    First Commit

---
+++ nftmarketplace/screens/Home.js
@@ -0,0 +1,12 @@
+import {Text, View, SafeArea, FlatList, SafeAreaView} from 'react-native';
+import { useState  } from 'react';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex:1}}>
+      
+    </SafeAreaView>
+  )
+}
+
+export default Home
